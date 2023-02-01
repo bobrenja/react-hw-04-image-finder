5336809e9ef06cd6cac4fa0dd2f21ab2fc56e808
bobrenja <abcd43@ukr.net>
2023-02-01 20:50:15 +0200

    add notify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import { Component } from 'react';
 import { CirclesWithBar } from 'react-loader-spinner';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { getImg } from './helpers/getApi';
+
 import Modal from './helpers/components/Modal/Modal';
 import ImgFullModal from './ImgFullModal/ImgFullModal';
 import './styles.css';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
-import css from "./app.module.scss"
+import css from './app.module.scss';
 
 const PAGE_ITEM = 12;
 class App extends Component {
@@ -22,7 +26,7 @@ class App extends Component {
     showModals: false,
     imgModal: null,
     btn: false,
-    spiner:false
+    spiner: false,
   };
 
   searchQueryImages = ({ search }) => {
@@ -36,33 +40,42 @@ class App extends Component {
   componentDidMount() {}
   componentDidUpdate(prevProps, prevState) {
     if (this.state.searchPixabay !== prevState.searchPixabay) {
-      this.resetState()
+      this.resetState();
       this.fetchImg();
     }
   }
 
   resetState() {
-    this.setState(({ images,btn,page }) => ({
+    this.setState({
       images: [],
       btn: false,
-      page: 0,
-    }));
+      page: 1,
+    });
   }
 
   fetchImg = async () => {
     try {
-      this.setState({ spiner: true, btn:false });
+      this.setState({ spiner: true, btn: false });
       const { searchPixabay, page } = this.state;
       let btnState = false;
 
       const data = await getImg(searchPixabay, page);
-      const totalPage = data.totalHits / PAGE_ITEM;
+      const totalPage = Math.ceil(data.totalHits / PAGE_ITEM);
+      console.log(page);
 
+      if (data.totalHits && page === 1) {
+        toast.success(`Search ${data.totalHits} image`);
+      }
+      if (!data.totalHits) {
+        toast.error(`No results "${searchPixabay}" `);
+      }
       console.log(data);
       if (page < totalPage) {
         btnState = true;
       }
-      this.setState(({ images }) => ({
+      console.log(page);
+
+      this.setState(({ images, page }) => ({
         images: [...images, ...data.hits],
         btn: btnState,
         page: page + 1,
@@ -105,18 +118,19 @@ class App extends Component {
         )}
         {this.state.btn && <Button btnClick={this.fetchImg} />}
         <CirclesWithBar
-  height="100"
-  width="100"
-  color="#4507ef"
-  justic-
-  wrapperStyle={{}}
-  wrapperClass={css.loader}
-  visible={this.state.spiner}
-  outerCircleColor=""
-  innerCircleColor=""
-  barColor="blue"
-  ariaLabel='circles-with-bar-loading'
-/>
+          height="100"
+          width="100"
+          color="#4507ef"
+          justic-
+          wrapperStyle={{}}
+          wrapperClass={css.loader}
+          visible={this.state.spiner}
+          outerCircleColor=""
+          innerCircleColor=""
+          barColor="blue"
+          ariaLabel="circles-with-bar-loading"
+        />
+        <ToastContainer />
       </div>
     );
   }
