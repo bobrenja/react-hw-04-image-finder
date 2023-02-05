2cd817a6e8abe1444ebc8dd1a922db706b93bb42
bobrenja <abcd43@ukr.net>
2023-02-05 22:06:30 +0200

    Update Modal.jsx

--- src/components/helpers/components/Modal/Modal.jsx
+++ src/components/helpers/components/Modal/Modal.jsx
@@ -1,21 +1,20 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import style from './Modal.module.scss';
 
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ close, children }) => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const closeModal = useCallback(({ target, currentTarget, code }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
       close();
     }
-  });
+  };
 
   useEffect(() => {
     document.addEventListener('keydown', closeModal);
     return () => document.removeEventListener('keydown', closeModal);
-  }, [closeModal]);
+  });
 
   return createPortal(
     <div className={style.overlay} onClick={closeModal}>
